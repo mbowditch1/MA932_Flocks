c2e3e17b24a766fedfeb253203ab51e5a78dc9ac
mbowditch1 <matthew.bowditch@warwick.ac.uk>
2023-04-02 21:20:09 +0100

    Started blender CSV generation

--- blender_scripts/animate_3d.py
+++ blender_scripts/animate_3d.py
@@ -1,13 +1,22 @@
 import bpy
+import csv
+
+positions = []
+
+with open("/home/mbowditch/Documents/MA932/blender_scripts/positions.csv", 'r') as file:
+  csvreader = csv.reader(file)
+  for row in csvreader:
+    positions.append([float(x) for x in row])
 
 col = bpy.data.collections[1].objects
 
 for x in col:
-    x.location = [0,0,0]
-    x.keyframe_insert(data_path = "location", frame=1)
-    x.location = [10,10,10]
-    x.keyframe_insert(data_path = "location", frame=50)
-    
+    f = 1
+    for p in positions:
+        x.location = p
+        x.keyframe_insert(data_path = "location", frame=f)
+        f += 1
+
 # For each 'bird' create an object in collection
 
 # Loop through objects in collection and set new locations
