737b7b789da50651ffef9faeb94ec8779b474664
mbowditch1 <matthew.bowditch@warwick.ac.uk>
2023-04-03 13:38:15 +0100

    Removed test.txt

--- blender_scripts/animate_3d.py
+++ blender_scripts/animate_3d.py
@@ -1,21 +1,29 @@
 import bpy
 import csv
 
-positions = []
-
+curr_positions = []
+all_positions = []
 with open("/home/mbowditch/Documents/MA932/blender_scripts/positions.csv", 'r') as file:
-  csvreader = csv.reader(file)
-  for row in csvreader:
-    positions.append([float(x) for x in row])
+    csvreader = csv.reader(file)
+    for row in csvreader:
+        if row[0] != "n":
+            curr_positions.append([float(x) for x in row])
+        else:
+            all_positions.append(curr_positions)
+            curr_positions = []
+
+
+for pos in all_positions:
+    f = 1
+    bpy.ops.mesh.primitive_ico_sphere_add(radius=0.5, location=pos[0])
 
 col = bpy.data.collections[1].objects
-
-for x in col:
+for i,x in enumerate(col):
     f = 1
-    for p in positions:
+    for p in all_positions[i]:
         x.location = p
         x.keyframe_insert(data_path = "location", frame=f)
-        f += 1
+        f += 5
 
 # For each 'bird' create an object in collection
 
